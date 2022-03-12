4682dcc3deffaa33348ac2dcec7f0be3c8093831
jbryans <jbryans2014@gmail.com>
2022-03-12 16:40:57 -0600

    finish

---
+++ todo.js
@@ -0,0 +1,49 @@
+    // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
+    
+
+    let arrayOfTodos = [
+        {
+        "userId": 14,
+        "id": 1,
+        "title": "delectus aut autem",
+        "completed": false
+    },
+    {
+        "userId": 20,
+        "id": 2,
+        "title": "delectus aut autem",
+        "completed": false
+    }]
+
+    console.log (arrayOfTodos[0].userId)
+    console.log (arrayOfTodos[1].userId)
+    
+
+
+    const fetchTodos = () => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+        .then( (response) => response.json())
+        .then( (json) => arrayOfTodos = json)
+    }
+    
+    const logTodos = () => {
+        console.log(arrayOfTodos)
+    }
+    
+    const populateTodos = () => {
+        console.log(arrayOfTodos)
+        let ol = document.getElementById('todo-list');
+        for (let index = 0; index < arrayOfTodos.length; index++) {
+            const obj = arrayOfTodos[index];
+            console.log(obj)
+            console.log(obj.title)
+            let li = document.createElement('li');
+            var title = document.createTextNode(obj.title);
+            ol.appendChild(li);
+            li.appendChild(title)
+        }
+        // let li = document.createElement('li');
+        // var title = document.createTextNode(obj.title);
+        // li.appendChild(ol);
+        // li.appendChild(title)
+    }
